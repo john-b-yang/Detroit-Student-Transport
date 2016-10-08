7ce79c2f78f11451b25624ccf4c05794a2a2d5c4
John Yang <byjohnyang@gmail.com>
2016-10-08 17:17:48 -0400

    Removed swift colors pod and added swift colors file

--- iOS/Pods/Target Support Files/Pods-Detroit-School-Transport/Pods-Detroit-School-Transport-frameworks.sh
+++ iOS/Pods/Target Support Files/Pods-Detroit-School-Transport/Pods-Detroit-School-Transport-frameworks.sh
@@ -85,9 +85,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/IQDropDownTextField/IQDropDownTextField.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SwiftHEXColors/SwiftHEXColors.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/IQDropDownTextField/IQDropDownTextField.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SwiftHEXColors/SwiftHEXColors.framework"
 fi
