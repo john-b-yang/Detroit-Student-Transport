0e3bc9da2704f781e3685668ff1d9961a0f9c233
harshita-gupta <gupta.harshita.001@gmail.com>
2016-10-08 19:39:39 -0400

    added gitignore for iOS settings files, initiated first request to API

---
+++ Detroit Data Mining/firebaseDataMine.py
@@ -0,0 +1,29 @@
+__author__ = 'Harshita'
+
+# import pickle
+# from bs4 import BeautifulSoup
+# from StringIO import StringIO
+# import pycurl
+# import re
+# from aenum import Enum
+
+# test firebase post 
+
+
+import json, requests, pprint
+
+url = 'http://ddot-beta.herokuapp.com/api/api/where/stops-for-route/DDOT_6419.json?key=MHACKS8'
+
+# params = dict(
+#     origin='Chicago,IL',
+#     destination='Los+Angeles,CA',
+#     waypoints='Joplin,MO|Oklahoma+City,OK',
+#     sensor='false'
+# )
+
+data = requests.get(url=url)
+binary = data.content
+output = json.loads(binary)
+
+for stop in output['data']['references']['stops']:
+    print stop['id']
