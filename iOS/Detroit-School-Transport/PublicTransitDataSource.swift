cf622e77be269b2a26f3d80df9b8049c694b999d
harshita-gupta <gupta.harshita.001@gmail.com>
2016-10-08 20:23:54 -0400

    Revert "adding extra file to protect to store Detroit Public Transit API Calls"
    This reverts commit 7a2e8d84176d1a0af26b5eeb039c9b4cf11be9b5.

--- iOS/Detroit-School-Transport/PublicTransitDataSource.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PublicTransitDataSource.swify.swift
-//  Detroit-School-Transport
-//
-//  Created by Harshita Gupta on 10/8/16.
-//  Copyright © 2016 John Yang. All rights reserved.
-//
-
-import Foundation
